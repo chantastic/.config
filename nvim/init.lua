12ac1ff6c7c0e7cef08d88429718b650e69736e9
Michael Chan <mijoch@gmail.com>
2024-09-28 13:45:49 -0700

    add supermaven

--- nvim/init.lua
+++ nvim/init.lua
@@ -927,6 +927,12 @@ require('lazy').setup({
       'nvim-lua/plenary.nvim',
     },
   },
+  {
+    'supermaven-inc/supermaven-nvim',
+    config = function()
+      require('supermaven-nvim').setup {}
+    end,
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
