385b630ea62396e98319710d9572375cb53d8952
Michael Chan <mijoch@gmail.com>
2024-09-29 17:47:25 -0700

    Add avante.nvim as custom plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -951,6 +951,8 @@ require('lazy').setup({
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
+  require 'custom.plugins.avante', -- adds gitsigns recommend keymaps
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
